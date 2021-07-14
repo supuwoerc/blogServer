0dbdf6282c5155ef63d4506fc7953e35ced47c52
zhangqm <2293172479@qq.com>
2021-07-14 20:02:50 +0800

    init resp

---
+++ src/main/java/cn/lookup/sanye/SanyeApplication.java
@@ -0,0 +1,13 @@
+package cn.lookup.sanye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lookup/sanye/SanyeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lookup.sanye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
